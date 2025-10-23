0ee45988c1dac5c99256dd1e436728d28df251ba
Kevin Huber <km.huber@outlook.com>
2025-10-23 20:09:48 +0000

    Final Commit

--- testdbphp.php
+++ testdbphp.php
@@ -1,7 +1,16 @@
-<?php
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Northland Analytics PHP/MySQL Test Page</title>
+</head>
+<body>
 
+<h1>Northland Analytics PHP/MySQL Test Page</h1>
+<p> Database Records Found</p>
+
+<?php
 //this is the php object oriented style of creating a mysql connection
-$conn = new mysqli('localhost', 'joeax', 'abc123', 'employees' );
+$conn = new mysqli('localhost', 'phpuser', 'php1234', 'employees' );
 
 //check for connection success
 if ($conn->connect_error) {
@@ -11,7 +20,7 @@ echo "MySQL Connection Succeeded<br><br>";
 
 //create the SQL select statement, notice the funky string concat at the end to variablize the query
 //based on using the GET attribute
-$sql = "SELECT first_name,last_name FROM employees where last_name = 'Weedman'";
+$sql = "SELECT first_name,last_name,hire_date FROM employees where last_name = 'Weedman'";
 
 //put the resultset into a variable, again object oriented way of doing things here
 $result = $conn->query($sql);
@@ -23,7 +32,7 @@ $result = $conn->query($sql);
 if ($result->num_rows > 0){
 //print rows of records found in the database if any
     while($row = $result->fetch_assoc()){
-         echo "Employee: " . $row["first_name"]. " " . $row["last_name"]. "<br>";
+         echo "Employee: " . $row["first_name"]. " " . $row["last_name"]. " Hire Date: " . $row["hire_date"] . " <br>";
    }
  } else {
          echo "No Records Found";
@@ -33,3 +42,9 @@ if ($result->num_rows > 0){
 $conn->close();
 
 ?>
+
+<p> For more information on connecting PHP to MySQL: 
+	<a href="https://www.php.net/manual/en/book.mysqli.php" target="_blank">Click Here</a>.
+</p>
+</body>
+</html>
